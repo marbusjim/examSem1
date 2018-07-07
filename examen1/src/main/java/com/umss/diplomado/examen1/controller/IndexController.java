82da31df70f03385352205edce7fcbc89a56748c
Marcos Bustos <marbusjim@gmail.com>
2018-07-06 22:05:21 -0400

    creacion de proyecto con spring y maven

---
+++ examen1/src/main/java/com/umss/diplomado/examen1/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.umss.diplomado.examen1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @RequestMapping("/")
+    public String index() {
+        return "Hola";
+    }
+}
