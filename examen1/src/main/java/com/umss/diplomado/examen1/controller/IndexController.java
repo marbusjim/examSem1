2b343b27c811b824705f51b924a82280b256c03e
Marcos Bustos <marbusjim@gmail.com>
2018-07-06 22:17:15 -0400

    primer cambio feature2

--- examen1/src/main/java/com/umss/diplomado/examen1/controller/IndexController.java
+++ examen1/src/main/java/com/umss/diplomado/examen1/controller/IndexController.java
@@ -9,4 +9,9 @@ public class IndexController {
     public String index() {
         return "Hola";
     }
+
+    @RequestMapping("/saludo2")
+    public String saludo1() {
+        return "Hola soy el saludo2";
+    }
 }
